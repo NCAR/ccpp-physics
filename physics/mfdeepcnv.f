bdd679a6d43419c967f941209c1f25a8acf47c1e
Dave Gill <gill@ucar.edu>
2017-08-29 15:57:01 -0600

    comments for each entry var, rename subrs, enclose in module

--- physics/mfdeepcnv.f
+++ physics/mfdeepcnv.f
@@ -1,4 +1,4 @@
-      module sasas_deep
+module sasas_deep
       contains
 
       subroutine sasasdeep_init
@@ -8,34 +8,37 @@
      &     q1,t1,u1,v1,cldwrk,rn,kbot,ktop,kcnv,islimsk,garea,
      &     dot,ncloud,ud_mf,dd_mf,dt_mf,cnvw,cnvc)
 !
-! | local var name | longname | units | rank | type |
-! | im
-! | ix
-! | km
-! | delt
-! | delp
-! | prslp
-! | psp | surface_air_pressure | Pa | 1 | real |
-! | phil
-! | ql
-! | q1
-! | t1
-! | u1
-! | v1
-! | cldwrk
-! | rn
-! | kbot
-! | ktop
-! | kcnv
-! | islimsk
-! | garea
-! | dot
-! | ncloud
-! | ud_mf
-! | dd_mf
-! | dt_mf
-! | cnvw
-! | cnvc
+! | local var name | longname                                              | description                   | units   | rank | type    |    kind   | intent |
+! |================|=======================================================|===============================|=========|======|=========|===========|========|
+! | im             | horizontal_dimension                                  | number of used points         | index   |    0 | integer |           | in     |
+! | ix             | horizontal_dimension                                  | horizontal dimension          | index   |    0 | integer |           | in     |
+! | km             | vertical_dimension                                    | vertical layer dimension      | index   |    0 | integer |           | in     |
+! | delt           | time_step                                             | physics time step             | s       |    0 | real    | kind_phys | in     |
+! | delp           | air_pressure_layer_difference                         | pres(k) - pres(k+1)           | Pa      | 2    | real    | kind_phys | in     |
+! | prslp          | air_pressure_layer                                    | mean layer presure            | Pa      | 2    | real    | kind_phys | in     |
+! | psp            | surface_air_pressure                                  | surface pressure              | Pa      | 1    | real    | kind_phys | in     |
+! | phil           | geopotential                                          | layer geopotential            | m2 s-2  | 2    | real    | kind_phys | in     |
+! | ql1            | cloud_ice_mixing_ratio                                | cloud ice                     | kg kg-1 | 2    | real    | kind_phys | inout  |
+! | ql2            | cloud_liquid_water_mixing_ratio                       | cloud water                   | kg kg-1 | 2    | real    | kind_phys | inout  |
+! | q1             |                                                       | updated tracers               | kg kg-1 | 2    | real    | kind_phys | inout  |
+! | t1             | air_temperature                                       | updated temperature           | K       | 2    | real    | kind_phys | inout  |
+! | u1             | grid_eastward_wind                                    | updated zonal wind            | m s-1   | 2    | real    | kind_phys | inout  |
+! | v1             | grid_northward_wind                                   | updated meridional wind       | m s-1   | 2    | real    | kind_phys | inout  |
+! | cldwrk         |                                                       | cloud work function           | m2 s-2  | 1    | real    | kind_phys |   out  |
+! | rn             | convective_rainfall_amount                            | convective rain               | m       | 1    | real    | kind_phys |   out  |
+! | kbot           | index_for_cloud_base                                  | index for cloud base          | index   | 1    | integer |           |   out  |
+! | ktop           | index_for_cloud_top                                   | index for cloud top           | index   | 1    | integer |           |   out  |
+! | kcnv           | flag_deep_convection                                  | deep convection: 0=no, 1=yes  | flag    | 1    | integer |           |   out  |
+! | islimsk        | sea_land_ice_mask                                     | landmask: sea/land/ice=0/1/2  | flag    | 1    | integer |           | in     |
+! | garea          |                                                       |description                    |         | 1    | real    | kind_phys | in     |
+! | dot            | omega                                                 | layer mean vertical velocity  | Pa s-1  | 2    | real    | kind_phys | in     |
+! | ncloud         |                                                       | number of cloud species       |         |    0 | integer |           | in     |
+! | ud_mf          | atmosphere_updraft_convective_mass_flux               | (updraft mass flux) * delt    | kg m-2  | 2    | real    | kind_phys |   out  |
+! | dd_mf          | atmosphere_downdraft_convective_mass_flux             | (downdraft mass flux) * delt  | kg m-2  | 2    | real    | kind_phys |   out  |
+! | dt_mf          | atmosphere_updraft_convective_mass_flux_at_cloud_top  | ud_mf at cloud top            | kg m-2  | 2    | real    | kind_phys |   out  |
+! | cnvw           | atmosphere_convective_cloud_water_mixing_ratio        | convective cloud water        | kg kg-1 | 2    | real    | kind_phys |   out  |
+! | cnvc           | cloud_binary_mask                                     | convective cloud cover        | flag    | 2    | real    | kind_phys |   out  |
+
 !
       use machine , only : kind_phys
       use funcphys , only : fpvs
