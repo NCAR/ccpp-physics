ba50b3c25753b0af1f882d6e4052c00b40d0994b
Dom Heinzeller <dom.heinzeller@noaa.gov>
2018-05-09 10:27:31 -0600

    Add CCPP build to compile.sh if MAKEOPT contains CCPP=Y. The CCPP build uses the Intel 18 compiler on theia, because model segfaults when trying to load libccpp.so at model startup with Intel 15 compiler

--- tests/compile.sh
+++ tests/compile.sh
@@ -26,16 +26,39 @@ echo "Compiling ${MAKE_OPT} into $BUILD_NAME.exe on $MACHINE_ID"
 
 # Configure NEMS
 cd $PATHNEMS
-src/configure configure.fv3.$MACHINE_ID $MACHINE_ID/fv3
+# Current Intel 15 compiler is incompatible with latest features needed
+# for CCPP (dynamic loading, ISO_C bindings etc.) and causes segfaults
+# when trying to load the shared library libccpp.so when launching fv3.exe
+if [[ "${MAKE_OPT}" == *"CCPP=Y"* && "${MACHINE_ID}" == "theia" ]]; then
+  src/configure configure.fv3.$MACHINE_ID $MACHINE_ID/fv3.intel-18.1.163
+else
+  src/configure configure.fv3.$MACHINE_ID $MACHINE_ID/fv3
+fi
 
 # Load the "module" command, purge modules, and load modules
 source $PATHNEMS/src/conf/modules.nems.sh
 module list
 
+# Build CCPP
+if [[ "${MAKE_OPT}" == *"CCPP=Y"* ]]; then
+  export readonly PATH_CCPP="$PATHTR/../ccpp"
+  export readonly NEMS_CCPP_CPPFLAGS="-DCCPP"
+  export readonly NEMS_CCPP_LDFLAGS="-L${PATH_CCPP}/lib -lccpp"
+  # Run ccpp_prebuild.py from the top-level directory before building the CCPP framework and physics
+  cd ${PATHTR}/..
+  if [[ "${MAKE_OPT}" == *"DEBUG=Y"* ]]; then
+    ./ccpp/framework/scripts/ccpp_prebuild.py --model=FV3v1 --debug
+  else
+    ./ccpp/framework/scripts/ccpp_prebuild.py --model=FV3v1
+  fi
+  # Build CCPP framework and physics
+  cd ${PATH_CCPP}
+  ./build_ccpp.sh ${MACHINE_ID} ${PATH_CCPP} "${MAKE_OPT}" ${clean_before} ${clean_after}
+fi
+
 # Copy configuration to FV3:
 cp -fp $PATHNEMS/src/conf/configure.nems $PATHTR/conf/configure.fv3
 cp -fp $PATHNEMS/src/conf/modules.nems $PATHTR/conf/modules.fv3
-
 
 # Build FV3
 cd $PATHTR
