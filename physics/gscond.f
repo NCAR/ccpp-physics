326a7b7ff1b7700971f2cb14911eeda25ca10777
Man.Zhang <Man.Zhang@noaa.gov>
2018-06-20 08:41:59 -0600

    merge gwdc_run with NEMSfv3gfs, and get B4B results.

--- physics/gscond.f
+++ physics/gscond.f
@@ -121,7 +121,7 @@
       real (kind=kind_phys)  qi(im), qint(im), ccrik, e0
      &,                      cond,   rdt, us, cclimit, climit
      &,                      tmt0, tmt15, qik, cwmik
-     &,                      ai, qw, u00ik, tik, pres, pp0, fi
+     &,                      ai, qw, u00ik, tik, pres, pp0, fi 
      &,                      at, aq, ap, fiw, elv, qc, rqik
      &,                      rqikk, tx1, tx2, tx3, es, qs
      &,                      tsq, delq, condi, cone0, us00, ccrik1
@@ -173,7 +173,7 @@
 !      endif
 !
 !*************************************************************
-!> -# Begining of  grid-scale condensation/evaporation loop (start of
+!> -# Begining of  grid-scale condensation/evaporation loop (start of 
 !! k-loop, i-loop)
 !*************************************************************
 !
@@ -182,7 +182,7 @@
 !       vprs(:) = 0.001 * fpvs(t(:,k))       ! fpvs in pa
 !-----------------------------------------------------------------------
 !------------------qw, qi and qint--------------------------------------
-        do i = 1, im
+        do i = 1, im                                    
           tmt0  = t(i,k)-273.16
           tmt15 = min(tmt0,cons_m15)
           qik   = max(q(i,k),epsq)
@@ -214,23 +214,23 @@
 !! cloud identification number IW, which is zero for cloud water and
 !! unity for cloud ice (Table 2 in 
 !! \cite zhao_and_carr_1997):
-!!  - All clouds are defined to consist of liquid water below the
-!! freezing level (\f$T\geq 0^oC\f$) and of ice particles above the
+!!  - All clouds are defined to consist of liquid water below the 
+!! freezing level (\f$T\geq 0^oC\f$) and of ice particles above the 
 !! \f$T=-15^oC\f$ level.
-!!  - In the temperature region between \f$-15^oC\f$ and \f$0^oC\f$,
-!! clouds may be composed of liquid water or ice. If there are cloud
+!!  - In the temperature region between \f$-15^oC\f$ and \f$0^oC\f$, 
+!! clouds may be composed of liquid water or ice. If there are cloud 
 !! ice particles above this point at the previous or current time step,
-!! or if the cloud at this point at the previous time step consists of
-!! ice particles, then the cloud substance at this point is considered
+!! or if the cloud at this point at the previous time step consists of 
+!! ice particles, then the cloud substance at this point is considered 
 !! to be ice particles because of the cloud seeding effect and the
-!! memory of its content. Otherwise, all clouds in this region are
+!! memory of its content. Otherwise, all clouds in this region are 
 !! considered to contain supercooled cloud water.
 
 !-------------------ice-water id number iw------------------------------
           if(tmt0.lt.-15.0) then
             u00ik = u(i,k)
-            fi    = qik - u00ik*qi(i)
-            if(fi > d00.or.cwmik > climit) then
+            fi    = qik - u00ik*qi(i)    
+            if(fi > d00.or.cwmik > climit) then                    
                iw(i,k) = 1
             else
               iw(i,k) = 0
@@ -251,8 +251,8 @@
 !> -# Condensation and evaporation of cloud
 !--------------condensation and evaporation of cloud--------------------
         do i = 1, im
-!>  - Compute the changes in t, q and p (\f$A_{t}\f$,\f$A_{q}\f$ and
-!! \f$A_{p}\f$) caused by all the processes except grid-scale
+!>  - Compute the changes in t, q and p (\f$A_{t}\f$,\f$A_{q}\f$ and 
+!! \f$A_{p}\f$) caused by all the processes except grid-scale 
 !! condensation and evaporation.
 !!\f[
 !!   A_{t}=(t-tp)/dt
@@ -274,7 +274,7 @@
           at    = (tik-tp(i,k)) * rdt
           aq    = (qik-qp(i,k)) * rdt
           ap    = (pres-pp0)    * rdt
-!>  - Calculate the saturation specific humidity \f$q_{s}\f$ and the
+!>  - Calculate the saturation specific humidity \f$q_{s}\f$ and the 
 !! relative humidity \f$f\f$ using IW.
 !----------------the satuation specific humidity------------------------
           fiw   = float(iwik)
@@ -283,7 +283,7 @@
 !     if (lprnt) print *,' qc=',qc,' qint=',qint(i),' qi=',qi(i)
 !----------------the relative humidity----------------------------------
           if(qc.le.1.0e-10) then
-            rqik=d00
+            rqik=d00 
           else
             rqik = qik/qc
           endif
@@ -295,30 +295,30 @@
 !!       b=1-\left ( \frac{f_{s}-f}{f_{s}-u} \right )^{1/2}
 !!\f]
 !! for \f$f>u\f$; and \f$b=0\f$ for \f$f<u\f$. where \f$f_{s}=1.0\f$ is
-!! the relative humidity in a cloud region and \f$u\f$ ,which is an
+!! the relative humidity in a cloud region and \f$u\f$ ,which is an 
 !! input parameter accounts for the effects of subgrid-scale variations
-!! in moisture on large-scale condensation. Since both temperature and
+!! in moisture on large-scale condensation. Since both temperature and 
 !! moisture may vary at scales smaller than the model grid scale, it is
 !! possible for condensation to occur before the grid-average relative
-!! humidity reaches 100%. Therefore \f$u\f$ needs to be less than 1.0
-!! to account for the subgrid-scale variation of temperature and
+!! humidity reaches 100%. Therefore \f$u\f$ needs to be less than 1.0 
+!! to account for the subgrid-scale variation of temperature and 
 !! moisture fields and allow subgrid-scale condensation.
-!!  - If cloud fraction \f$b\leq 1.0\times10^{-3}\f$, then evaporate
+!!  - If cloud fraction \f$b\leq 1.0\times10^{-3}\f$, then evaporate 
 !! any existing cloud condensate using evaporation rate \f$E_{c}\f$ as
 !! computed below.
-!!\n If \f$q_{0}\f$ represents the specific humidity at relative
+!!\n If \f$q_{0}\f$ represents the specific humidity at relative 
 !! humidity \f$u\f$, then
 !!\f[
 !!           q_{0}=uq_{s}
 !!\f]
-!!\n if the cloud water/ice at this point is enough to be evaporated
-!! until \f$u\f$ is reached, then the evaporation rate \f$E_{c}\f$,
+!!\n if the cloud water/ice at this point is enough to be evaporated 
+!! until \f$u\f$ is reached, then the evaporation rate \f$E_{c}\f$, 
 !! assuming that the evaporation process occurs in one time step, is
 !! determined by
 !!\f[
 !!           E_{c}=\frac{q_{0}-q}{dt}
 !!\f]
-!!\n  Using \f$q_{0}=uq_{s}\f$ and the equation \f$q=fq_{s}\f$,
+!!\n  Using \f$q_{0}=uq_{s}\f$ and the equation \f$q=fq_{s}\f$, 
 !! \f$E_{c}\f$ then becomes
 !!\f[
 !!  E_{c}=\frac{q_{s}}{dt}(u-f)
@@ -337,7 +337,7 @@
 !! Clausius-Clapeyron equation \f$de_{s}/dT=\epsilon Le_{s}/RT^{2}\f$,
 !! where \f$q_{s}\f$ is the saturation specific humidity,\f$e_{s}\f$
 !! is the saturation vapor pressure, \f$R\f$ is the specific gas
-!! constant for dry air, \f$f\f$ is the relative humidity, and
+!! constant for dry air, \f$f\f$ is the relative humidity, and 
 !! \f$\epsilon=0.622\f$, the expression for \f$C_{g}\f$ has the form
 !!\f[
 !!  C_{g}=\frac{M-q_{s}f_{t}}{1+(f\epsilon L^{2}q_{s}/RC_{p}T^{2})}+E_{c}
@@ -350,7 +350,7 @@
 !! \f$f_{t}\f$ was derived by 
 !! \cite sundqvist_et_al_1989 using the hypothesis that the quantity
 !! \f$M+E_{c}\f$ is divided into one part,\f$bM\f$,which condenses
-!! in the already cloudy portion of a grid square, and another part,
+!! in the already cloudy portion of a grid square, and another part, 
 !! \f$(1-b)M+E_{c}\f$,which is used to increase the relative humidity
 !! of the cloud-free portion and the cloudiness in the square. The
 !! equation is written as
@@ -368,7 +368,7 @@
 !!\f[
 !!   t=t+\frac{L}{C_{p}}(C_{g}-E_{c})\times dt
 !!\f]
-!!\n where \f$L\f$ is the latent heat of condensation/deposition, and
+!!\n where \f$L\f$ is the latent heat of condensation/deposition, and 
 !! \f$C_{p}\f$ is the specific heat of air at constant pressure.
 
 !----------------cloud cover ratio ccrik--------------------------------
@@ -389,7 +389,7 @@
 !   if no cloud exists then evaporate any existing cloud condensate
 !----------------evaporation of cloud water-----------------------------
           e0 = d00
-          if (ccrik <= cclimit.and. cwmik > climit)  then
+          if (ccrik <= cclimit.and. cwmik > climit)  then 
 !
 !   first iteration - increment halved
 !
@@ -438,7 +438,7 @@
           cond = d00
 !         if (ccrik .gt. 0.20 .and. qc .gt. epsq) then
           if (ccrik .gt. cclimit .and. qc .gt. epsq) then
-             us00   = us  - u00ik
+             us00   = us  - u00ik 
              ccrik1 = 1.0 - ccrik
              aa     = eps*elv*pres*qik
              ab     = ccrik*ccrik1*qc*us00
