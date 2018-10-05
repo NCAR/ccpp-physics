79d6d5eafa27f4b982494519392215ea6512a6ee
climbfuji <dom.heinzeller@icloud.com>
2018-10-05 17:20:06 -0600

    physics/cu_gf_driver_pre.F90: add private/public statements, adjust indents, replace DDTs with Fortran standard variables (components of the DDTs), add interstitial code previously in GFS_physics_driver.F90

--- physics/cu_gf_driver_pre.F90
+++ physics/cu_gf_driver_pre.F90
@@ -1,62 +1,82 @@
 !> \file cu_gf_driver_pre.F90
 !!  Contains code related to GF convective schemes to be used within the GFS physics suite.
 
-      module cu_gf_driver_pre
+module cu_gf_driver_pre
 
-      contains
+   implicit none
 
-      subroutine cu_gf_driver_pre_init ()
-      end subroutine cu_gf_driver_pre_init
+   private
 
-      subroutine cu_gf_driver_pre_finalize()
-      end subroutine cu_gf_driver_pre_finalize
+   public :: cu_gf_driver_pre_init, cu_gf_driver_pre_run, cu_gf_driver_pre_finalize
+
+   contains
+
+   subroutine cu_gf_driver_pre_init ()
+   end subroutine cu_gf_driver_pre_init
+
+   subroutine cu_gf_driver_pre_finalize()
+   end subroutine cu_gf_driver_pre_finalize
 
 !> \section arg_table_cu_gf_driver_pre_run Argument Table
-!! | local_name     | standard_name                                          | long_name                                                                | units         | rank | type              |    kind   | intent | optional |
-!! |----------------|--------------------------------------------------------|--------------------------------------------------------------------------|---------------|------|-------------------|-----------|--------|----------|
-!! | Model          | FV3-GFS_Control_type                                   | Fortran DDT containing FV3-GFS model control parameters                  | DDT           |    0 | GFS_control_type  |           | in     | F        |
-!! | Statein        | FV3-GFS_Statein_type                                   | Fortran DDT containing FV3-GFS prognostic state in from dycore           | DDT           |    0 | GFS_statein_type  |           | in     | F        |
-!! | Grid           | FV3-GFS_Grid_type                                      | Fortran DDT containing FV3-GFS grid and interpolation related data       | DDT           |    0 | GFS_grid_type     |           | in     | F        |
-!! | Tbd            | FV3-GFS_Tbd_type                                       | Fortran DDT containing FV3-GFS miscellaneous data                        | DDT           |    0 | GFS_tbd_type      |           | inout  | F        |
-!! | errmsg         | error_message                                          | error message for error handling in CCPP                                 | none          |    0 | character         | len=*     | out    | F        |
-!! | errflg         | error_flag                                             | error flag for error handling in CCPP                                    | flag          |    0 | integer           |           | out    | F        |
+!! | local_name     | standard_name                                          | long_name                                        | units         | rank | type      |    kind   | intent | optional |
+!! |----------------|--------------------------------------------------------|--------------------------------------------------|---------------|------|-----------|-----------|--------|----------|
+!! | kdt            | index_of_time_step                                     | current forecast iteration                       | index         |    0 | integer   |           | in     | F        |
+!! | fhour          | forecast_time                                          | curent forecast time                             | h             |    0 | real      | kind_phys | in     | F        |
+!! | dtp            | time_step_for_physics                                  | physics timestep                                 | s             |    0 | real      | kind_phys | in     | F        |
+!! | t              | air_temperature                                        | model layer mean temperature                     | K             |    2 | real      | kind_phys | in     | F        |
+!! | q              | water_vapor_specific_humidity                          | water vapor specific humidity                    | kg kg-1       |    2 | real      | kind_phys | in     | F        |
+!! | prevst         | temperature_from_previous_timestep                     | temperature from previous time step              | K             |    2 | real      | kind_phys | in     | F        |
+!! | prevsq         | moisture_from_previous_timestep                        | moisture from previous time step                 | kg kg-1       |    2 | real      | kind_phys | in     | F        |
+!! | forcet         | temperature_tendency_due_to_dynamics                   | temperature tendency due to dynamics only        | K s-1         |    2 | real      | kind_phys | out    | F        |
+!! | forceq         | moisture_tendency_due_to_dynamics                      | moisture tendency due to dynamics only           | kg kg-1 s-1   |    2 | real      | kind_phys | out    | F        |
+!! | cactiv         | conv_activity_counter                                  | convective activity memory                       | none          |    1 | integer   |           | out    | F        |
+!! | conv_act       | gf_memory_counter                                      | Memory counter for GF                            | none          |    1 | real      | kind_phys | in     | F        |
+!! | errmsg         | ccpp_error_message                                     | error message for error handling in CCPP         | none          |    0 | character | len=*     | out    | F        |
+!! | errflg         | ccpp_error_flag                                        | error flag for error handling in CCPP            | flag          |    0 | integer   |           | out    | F        |
 !!
-    subroutine cu_gf_driver_pre_run (Model, Statein, Grid, Tbd, errmsg, errflg)
+   subroutine cu_gf_driver_pre_run (kdt, fhour, dtp, t, q, prevst, prevsq, forcet, forceq, cactiv, conv_act, errmsg, errflg)
 
-      use machine,               only: kind_phys
-      use GFS_typedefs,          only: GFS_control_type, GFS_statein_type, GFS_grid_type, GFS_tbd_type
+      use machine, only: kind_phys
 
       implicit none
 
-      type(GFS_control_type),           intent(in) :: Model
-      type(GFS_statein_type),           intent(in) :: Statein
-      type(GFS_grid_type),              intent(in) :: Grid
-      type(GFS_tbd_type),            intent(inout) :: Tbd
-
-      !--- hli for GF convective scheme
-      real(kind=kind_phys) :: dtdyn
-      !--
+      integer,          intent(in)  :: kdt
+      real(kind_phys),  intent(in)  :: fhour
+      real(kind_phys),  intent(in)  :: dtp
+      real(kind_phys),  intent(in)  :: t(:,:)
+      real(kind_phys),  intent(in)  :: q(:,:)
+      real(kind_phys),  intent(in)  :: prevst(:,:)
+      real(kind_phys),  intent(in)  :: prevsq(:,:)
+      real(kind_phys),  intent(out) :: forcet(:,:)
+      real(kind_phys),  intent(out) :: forceq(:,:)
+      integer,          intent(out) :: cactiv(:)
+      real(kind_phys),  intent(in)  :: conv_act(:)
       character(len=*), intent(out) :: errmsg
-      integer, intent(out) :: errflg
+      integer,          intent(out) :: errflg
+
+      ! local variables
+      real(kind=kind_phys) :: dtdyn
 
       ! Initialize CCPP error handling variables
       errmsg = ''
       errflg = 0
 
-      if(Model%kdt.gt.1) then
-       dtdyn=3600.0*(Model%fhour)/Model%kdt   ! G. Firl's suggestion
-       if(Model%dtp > dtdyn) then
-        Tbd%forcet(:,:)=(Statein%tgrs(:,:)  - Tbd%prevst(:,:))/Model%dtp
-        Tbd%forceq(:,:)=(Statein%qgrs(:,:,1)- Tbd%prevsq(:,:))/Model%dtp
-       else
-        Tbd%forcet(:,:)=(Statein%tgrs(:,:)  - Tbd%prevst(:,:))/dtdyn
-        Tbd%forceq(:,:)=(Statein%qgrs(:,:,1)- Tbd%prevsq(:,:))/dtdyn
-       endif
+      if(kdt.gt.1) then
+        dtdyn=3600.0*(fhour)/kdt
+        if(dtp > dtdyn) then
+          forcet(:,:)=(t(:,:) - prevst(:,:))/dtp
+          forceq(:,:)=(q(:,:) - prevsq(:,:))/dtp
+        else
+          forcet(:,:)=(t(:,:) - prevst(:,:))/dtdyn
+          forceq(:,:)=(q(:,:) - prevsq(:,:))/dtdyn
+        endif
       else
-       Tbd%forcet(:,:)=0.0
-       Tbd%forceq(:,:)=0.0
+        forcet(:,:)=0.0
+        forceq(:,:)=0.0
       endif
 
-    end subroutine cu_gf_driver_pre_run
+      cactiv(:)=nint(conv_act(:))
 
-    end module cu_gf_driver_pre
+   end subroutine cu_gf_driver_pre_run
+
+end module cu_gf_driver_pre
