7d6d6e2e1048e54175cb1de2ba2ceafffed53370
Dom Heinzeller <dom.heinzeller@noaa.gov>
2018-12-12 16:30:01 +0000

    conf/before_components.mk: bugfix, add missing switch to CCPP-compliant modulefile for Intel15 on Theia

--- conf/before_components.mk
+++ conf/before_components.mk
@@ -21,6 +21,11 @@ ifneq (,$(findstring INTEL18=Y,$(FV3_MAKEOPT)))
     override CHOSEN_MODULE=$(BUILD_TARGET)/fv3.intel-18.0.1.163
     $(warning Overriding CHOSEN_MODULE with $(CHOSEN_MODULE) as requested per MAKEOPT)
   endif
+else ifneq (,$(findstring CCPP=Y,$(FV3_MAKEOPT)))
+  ifeq ($(CHOSEN_MODULE),theia.intel/fv3)
+    override CHOSEN_MODULE=$(BUILD_TARGET)/fv3.intel-15.1.133
+    $(warning Overriding CHOSEN_MODULE with $(CHOSEN_MODULE) as requested per MAKEOPT)
+  endif
 endif
 
 CONFIGURE_NEMS_FILE=configure.fv3.$(BUILD_TARGET)
