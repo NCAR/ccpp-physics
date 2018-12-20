328e88170df23df33a893433670af45c19c6ba39
climbfuji <dom.heinzeller@icloud.com>
2018-12-20 11:26:32 -0700

    physics/radiation_clouds.f: bugfix, use correct effective radius for snow instead of ice for Thompson microphysics

--- physics/radiation_clouds.f
+++ physics/radiation_clouds.f
@@ -2686,7 +2686,7 @@
           clouds(i,k,6) = crp(i,k)  ! added for Thompson 
           clouds(i,k,7) = rer(i,k)
           clouds(i,k,8) = csp(i,k)  ! added for Thompson 
-          clouds(i,k,9) = rei(i,k)
+          clouds(i,k,9) = res(i,k)
         enddo
       enddo
 
