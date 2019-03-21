824009254877c1ac359b974ac1449506a98cf3a2
Dustin Swales <dustin.swales@noaa.gov>
2019-03-21 16:57:34 -0600

    Ported RRTMGP development from release repo. LW is working.

---
+++ physics/rrtmgp_lw_post.F90
@@ -0,0 +1,102 @@
+!>\file rrtmgp_lw_post
+!!This file contains
+      module rrtmgp_lw_post 
+      contains
+
+!>\defgroup rrtmgp_lw_post GFS RRTMGP scheme post
+!! @{
+!> \section arg_table_rrtmgp_lw_post_init Argument Table
+!!
+      subroutine rrtmgp_lw_post_init()
+      end subroutine rrtmgp_lw_post_init
+
+! PGI compiler does not accept lines longer than 264 characters, remove during pre-processing
+#ifndef __PGI
+!> \section arg_table_rrtmgp_lw_post_run Argument Table
+!! | local_name      | standard_name                                                                                 | long_name                                                                    | units    | rank |  type                 |   kind    | intent    | optional |
+!! |-----------------|-----------------------------------------------------------------------------------------------|------------------------------------------------------------------------------|----------|------|-----------------------|-----------|-----------|----------|
+!! | Model           | GFS_control_type_instance                                                                     | Fortran DDT containing FV3-GFS model control parameters                      | DDT      |    0 | GFS_control_type      |           | in        | F        |
+!! | Grid            | GFS_grid_type_instance                                                                        | Fortran DDT containing FV3-GFS grid and interpolation related data           | DDT      |    0 | GFS_grid_type         |           | in        | F        |
+!! | Radtend         | GFS_radtend_type_instance                                                                     | Fortran DDT containing FV3-GFS fields targetted for diagnostic output        | DDT      |    0 | GFS_radtend_type      |           | inout     | F        |
+!! | Coupling        | GFS_coupling_type_instance                                                                    | Fortran DDT containing FV3-GFS fields to/from coupling with other components | DDT      |    0 | GFS_coupling_type     |           | inout     | F        |
+!! | im              | horizontal_loop_extent                                                                        | horizontal loop extent                                                       | count    |    0 | integer               |           | in        | F        |
+!! | ltp             | extra_top_layer                                                                               | extra top layers                                                             | none     |    0 | integer               |           | in        | F        |
+!! | lm              | vertical_layer_dimension_for_radiation                                                        | number of vertical layers for radiation calculation                          | count    |    0 | integer               |           | in        | F        |
+!! | kd              | vertical_index_difference_between_inout_and_local                                             | vertical index difference between in/out and local                           | index    |    0 | integer               |           | in        | F        |
+!! | tsfa            | surface_air_temperature_for_radiation                                                         | lowest model layer air temperature for radiation                             | K        |    1 | real                  | kind_phys | in        | F        |
+!! | htlwc           | tendency_of_air_temperature_due_to_longwave_heating_on_radiation_time_step                    | total sky heating rate due to longwave radiation                             | K s-1    |    2 | real                  | kind_phys | in        | F        |
+!! | htlw0           | tendency_of_air_temperature_due_to_longwave_heating_assuming_clear_sky_on_radiation_time_step | clear sky heating rate due to longwave radiation                             | K s-1    |    2 | real                  | kind_phys | in        | F        |
+!! | errmsg          | ccpp_error_message                                                                            | error message for error handling in CCPP                                     | none     |    0 | character             | len=*     | out       | F        |
+!! | errflg          | ccpp_error_flag                                                                               | error flag for error handling in CCPP                                        | flag     |    0 | integer               |           | out       | F        |
+!!
+#endif
+      subroutine rrtmgp_lw_post_run (Model, Grid, Radtend, Coupling,   &
+                 im, ltp, lm, kd, tsfa, htlwc, htlw0, errmsg, errflg)
+    
+      use machine,                   only: kind_phys
+      use GFS_typedefs,              only: GFS_coupling_type,          &
+                                           GFS_control_type,           &
+                                           GFS_grid_type,              &
+                                           GFS_radtend_type
+      implicit none
+      type(GFS_control_type),         intent(in)    :: Model
+      type(GFS_coupling_type),        intent(inout) :: Coupling
+      type(GFS_grid_type),            intent(in)    :: Grid
+      type(GFS_radtend_type),         intent(inout) :: Radtend
+      integer,                        intent(in)    :: im, ltp, LM, kd
+      real(kind=kind_phys), dimension(size(Grid%xlon,1), Model%levr+LTP), intent(in) ::  htlwc
+      real(kind=kind_phys), dimension(size(Grid%xlon,1), Model%levr+LTP), intent(in) ::  htlw0
+      real(kind=kind_phys), dimension(size(Grid%xlon,1)),                 intent(in) ::  tsfa
+      character(len=*), intent(out) :: errmsg
+      integer,          intent(out) :: errflg
+      ! local variables
+      integer :: k1, k
+
+      ! Initialize CCPP error handling variables
+      errmsg = ''
+      errflg = 0
+
+      if (Model%lslwr) then
+!> -# Save calculation results
+!>  - Save surface air temp for diurnal adjustment at model t-steps
+
+        Radtend%tsflw (:) = tsfa(:)
+
+        do k = 1, LM
+          k1 = k + kd
+            Radtend%htrlw(1:im,k) = htlwc(1:im,k1)
+        enddo
+        ! --- repopulate the points above levr
+        if (lm < Model%levs) then
+          do k = lm,Model%levs
+            Radtend%htrlw (1:im,k) = Radtend%htrlw (1:im,LM)
+          enddo
+        endif
+
+        if (Model%lwhtr) then
+          do k = 1, lm
+            k1 = k + kd
+            Radtend%lwhc(1:im,k) = htlw0(1:im,k1)
+          enddo
+          ! --- repopulate the points above levr
+          if (lm < Model%levs) then
+            do k = lm,Model%levs
+              Radtend%lwhc(1:im,k) = Radtend%lwhc(1:im,LM)
+            enddo
+          endif
+        endif
+
+! --- radiation fluxes for other physics processes
+        Coupling%sfcdlw(:) = Radtend%sfcflw(:)%dnfxc
+
+      endif                                ! end_if_lslwr
+
+      end subroutine rrtmgp_lw_post_run
+
+!> \section arg_table_rrtmgp_lw_post_finalize Argument Table
+!!
+      subroutine rrtmgp_lw_post_finalize ()
+      end subroutine rrtmgp_lw_post_finalize
+
+!! @}
+      end module rrtmgp_lw_post
