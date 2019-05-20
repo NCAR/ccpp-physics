47a28aa1fe2e33a0fb4f2c99c6e68c7c32a5e301
Dom Heinzeller <dom.heinzeller@noaa.gov>
2019-05-20 10:51:21 -0600

    Reverse whitespace changes to NEMSfv3gfs code

--- conf/before_components.mk
+++ conf/before_components.mk
@@ -24,8 +24,7 @@ ifneq (,$(findstring INTEL18=Y,$(FV3_MAKEOPT)))
     override CHOSEN_MODULE=$(BUILD_TARGET)/fv3.intel-18.0.3.222
     $(warning Overriding CHOSEN_MODULE with $(CHOSEN_MODULE) as requested per MAKEOPT)
   endif
-#else ifneq (,$(findstring CCPP=Y,$(FV3_MAKEOPT)))
-else ifneq (,$(or $(findstring CCPP=Y,$(COMPONENTS)),$(findstring CCPP=Y,$(FV3_MAKEOPT))))
+else ifneq (,$(findstring CCPP=Y,$(FV3_MAKEOPT)))
   ifeq ($(CHOSEN_MODULE),theia.intel/fv3)
     override CHOSEN_MODULE=$(BUILD_TARGET)/fv3.intel-15.1.133
     $(warning Overriding CHOSEN_MODULE with $(CHOSEN_MODULE) as requested per MAKEOPT)
