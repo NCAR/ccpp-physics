d0d40355c48ea3741aa5d365145d7b5c68e0c8e7
Man Zhang <Man.Zhang@noaa.gov>
2019-07-31 10:09:44 -0600

    initialize FER_HIRES implementation

---
+++ physics/module_mp_FER_HIRES_pre.F90
@@ -0,0 +1,164 @@
+!>\file module_mp_FER_HIRES_pre.F90
+!! This file contains a wrap around UPDATE_WATER in NAM/module_MICROPHYSICS.F90
+!! which should be called the first in the physics suite using Ferrier-Aligo MP
+!! scheme.
+
+!> This module contains the CCPP-compliant UPDATE_WATER for Ferrier-Aligo MP.
+     module module_fa_update_water
+
+     implicit none
+
+     private
+
+     public :: module_fa_update_water_init, module_fa_update_water_run, &
+               module_fa_update_water_finalize
+
+     contains
+
+!>\section arg_table_module_fa_update_water_init Argument Table
+!!
+     subroutine module_fa_update_water_init()
+     end subroutine module_fa_update_water_init
+
+!>\section arg_table_module_fa_update_water_finalize Argument Table
+!!
+     subroutine module_fa_update_water_finalize()
+     end subroutine module_fa_update_water_finalize
+
+!>\defgroup hafs_fa HAFS Ferrier-Aligo MP Scheme Update Water Module
+!! This subroutine is to update water array with CWM, F_RAIN, and F_ICE 
+!! for Ferrier-Aligo MP scheme. 
+!>\section arg_table_module_fa_update_water_run Argument Table
+!! | local_name     | standard_name                                         | long_name                                                                                  | units   | rank | type      | kind      | intent | optional |
+!! |----------------|-------------------------------------------------------|--------------------------------------------------------------------------------------------|---------|------|-----------|-----------|--------|----------|
+!! | cwm            | total_cloud_condensate_mixing_ratio                   | total cloud condensate mixing ratio (except water vapor) in NAM                            | kg kg-1 |    2 | real      | kind_phys | inout  | F        |
+!! | f_ice          | mass_fraction_of_ice_water_cloud                      | mass fraction of ice water cloud                                                           | frac    |    2 | real      | kind_phys | inout  | F        |
+!! | f_rain         | mass_fraction_of_rain_water_cloud                     | mass fraction of rain water cloud                                                          | frac    |    2 | real      | kind_phys | inout  | F        |
+!! | epsq           | minimum_value_of_specific_humidity                    | floor value for specific humidity                                                          | kg kg-1 |    0 | real      | kind_phys | in     | F        |
+!! | t              | air_temperature                                       | model layer mean temperature                                                               | K       |    2 | real      | kind_phys | inout  | F        |                 
+!! | qc             | cloud_condensed_water_mixing_ratio_updated_by_physics | moist (dry+vapor, no condensates) mixing ratio of cloud condensed water updated by physics | kg kg-1 |    2 | real      | kind_phys | inout  | F        | 
+!! | qr             | rain_water_mixing_ratio_updated_by_physics            | moist (dry+vapor, no condensates) mixing ratio of rain water updated by physics            | kg kg-1 |    2 | real      | kind_phys | inout  | F        |
+!! | qs             | snow_water_mixing_ratio_updated_by_physics            | moist (dry+vapor, no condensates) mixing ratio of snow water updated by physics            | kg kg-1 |    2 | real      | kind_phys | inout  | F        | 
+!! | qi             | ice_water_mixing_ratio_updated_by_physics             | moist (dry+vapor, no condensates) mixing ratio of ice water updated by physics             | kg kg-1 |    2 | real      | kind_phys | inout  | F        | 
+!! | qg             | graupel_mixing_ratio_updated_by_physics               | moist (dry+vapor, no condensates) mixing ratio of graupel updated by physics               | kg kg-1 |    2 | real      | kind_phys | inout  | F        | 
+!! | spec_adv       | flag_for_individual_cloud_species_advected            | flag for individual cloud species advected                                                 | flag    |    0 | logical   |           | in     | F        |
+!! | kdt            | index_of_time_step                                    | current forecast interation                                                                | index   |    0 | integer   |           | in     | F        |
+!! | lm             | vertical_dimension                                    | number of vertical levels                                                                  | count   |    0 | integer   |           | in     | F        |
+!! | ime            | horizontal_dimension                                  | horizontal dimension                                                                       | count   |    0 | integer   |           | in     | F        | 
+!! | errmsg         | ccpp_error_message                                    | error message for error handling in CCPP                                                   | none    |    0 | character | len=*     | out    | F        | 
+!! | errflg         | ccpp_error_flag                                       | error flag for error handling in CCPP                                                      | flag    |    0 | integer   |           | out    | F        |
+!!
+!! \section genal_update_water HAFS Update_Water General Algorithm
+     subroutine module_fa_update_water_run(CWM,F_ICE,F_RAIN,            & ! F_RIMEF  
+                             ,EPSQ                                      & 
+                             ,T,QC,QR,QS,QI,QG                          &
+                             ,SPEC_ADV,kdt                              &
+                             ,LM,IME,errmsg,errflg )
+!***********************************************************************
+!$$$  SUBPROGRAM DOCUMENTATION BLOCK
+!                .      .    .
+! SUBPROGRAM:    UPDATE_WATER          UPDATE WATER ARRAY
+!   PRGRMMR: FERRIER         ORG: NP22     DATE: 3 AUG 2009
+!
+! ABSTRACT:
+!     UPDATE WATER ARRAY FOR FERRIER MICROPHYSICS
+!
+! PROGRAM HISTORY LOG (with changes to called routines) :
+!   2009-08     FERRIER     - Synchronize WATER array with CWM, F_rain, F_ice arrays
+!
+! ATTRIBUTES:
+!   LANGUAGE: FORTRAN 90
+!-----------------------------------------------------------------------
+
+       USE MACHINE , only : kind_phys
+       IMPLICIT NONE
+     
+       REAL(KIND=KIND_PHYS) :: EPSQ
+
+!----------------------
+!-- Argument Variables
+!----------------------
+!
+!      INTEGER,INTENT(IN) :: NTIMESTEP,LM,IME
+       INTEGER,INTENT(IN) :: KDT,LM,IME
+!
+      LOGICAL,INTENT(IN) :: SPEC_ADV
+!
+      REAL(kind=kind_phys),DIMENSION(1:IME,1:LM),INTENT(INOUT) :: CWM   &
+                                                           ,F_ICE       &
+                                                           ,F_RAIN      &
+                                                           ,T,QC,QR     &
+                                                           ,QS,QI,QG
+!
+!--------------------
+!--  Local Variables
+!--------------------
+!
+      INTEGER :: I,J,K
+      REAL(kind=kind_phys) :: FRACTION, LIQW, OLDCWM
+      LOGICAL :: CLD_INIT
+      LOGICAL :: deep_ice
+      character(len=*), intent(out) :: errmsg
+      integer,          intent(out) :: errflg
+!
+!-----------------------------------------------------------------------
+!***********************************************************************
+!-----------------------------------------------------------------------
+
+! Initialize CCPP error handling variables
+      errmsg = ''
+      errflg = 0
+
+!
+!zm      IF(NTIMESTEP<=1)THEN
+      IF(kdt<=2)THEN
+        CLD_INIT=.TRUE.
+      ELSE
+        CLD_INIT=.FALSE.
+      ENDIF
+
+
+      IF (.NOT.SPEC_ADV .OR. CLD_INIT) THEN
+!-- Update WATER arrays when advecting only total condensate (spec_adv=F)
+!   or at the initial time step
+            DO K=1,LM
+!zm             DO J=JMS,JME
+              DO I=1,IME
+                IF (CWM(I,J,K)>EPSQ) THEN
+                  LIQW=(1.-F_ice(I,J,K))*CWM(I,J,K)
+                  QC(I,J,K)=(1.-F_rain(I,J,K))*LIQW
+                  QR(I,J,K)=F_rain(I,J,K)*LIQW
+                  QS(I,J,K)=F_ice(I,J,K)*CWM(I,J,K)
+                ELSE
+                  QC(I,J,K)=0.
+                  QR(I,J,K)=0.
+                  QS(I,J,K)=0.
+                ENDIF
+              ENDDO
+             ENDDO
+            ENDDO
+
+      ELSE 
+!-- Update CWM,F_ICE,F_RAIN arrays from separate species advection (spec_adv=T)
+            DO K=1,LM
+!zm             DO J=JMS,JME
+              DO I=1,IME
+                CWM(I,J,K)=QC(I,J,K)+QR(I,J,K)+QS(I,J,K)
+                IF (QS(I,J,K)>EPSQ) THEN
+                  F_ICE(I,J,K)=QS(I,J,K)/CWM(I,J,K)
+                ELSE
+                  F_ICE(I,J,K)=0.0
+                ENDIF
+                IF (QR(I,J,K)>EPSQ) THEN
+                  F_RAIN(I,J,K)=QR(I,J,K)/(QC(I,J,K)+QR(I,J,K))
+                ELSE
+                  F_RAIN(I,J,K)=0.
+                ENDIF
+              ENDDO
+             ENDDO
+            ENDDO
+      ENDIF 
+
+      end module_fa_update_water_run
+
+      end module module_fa_update_water
--- physics/mp_fer_hires.F90
+++ physics/mp_fer_hires.F90
@@ -78,17 +78,20 @@ module mp_fer_hires
        kme = nlev
        kte = nlev
 
-! FERRIER_INIT_HR in NAM_typedefs.F90
+!MZ: FERRIER_INIT_HR in NAM_typedefs.F90
+       Model%F_QC=.TRUE.
+       Model%F_QR=.TRUE.
+       Model%F_QS=.TRUE.
+       Model%F_QG=.TRUE.
+       !MZ NPRECIP- number of dynamics timesteps between calls to 
+       !convection and microphysics
+       DT_MICRO=Model%NPRECIP*Model%dtp
+
+       CALL FERRIER_INIT_HR(DT_MICRO)
 
        
-       ! Call init to initialize constans & lookup tables for microphysics
-       CALL fer_hires_init (MPDT,DT,DX,DY,LOWLYR,restart,         &
-                            allowed_to_read,                        &
-                            ids, ide, jds, jde, kds, kde,           &
-                            ims, ime, jms, jme, kms, kme,           &
-                            its, ite, jts, jte, kts, kte,           &
-                            F_ICE_PHY,F_RAIN_PHY,F_RIMEF_PHY)
 
+       if (errflg /= 0 ) return
        
        is_initialized = .true
 
@@ -96,7 +99,7 @@ module mp_fer_hires
      end subroutine mp_fer_hires_init
 
 
-
+!> This is the CCPP-compliant FER_HIRES driver module.
       subroutine mp_fer_hires_run ()
    
       implicit none
@@ -112,57 +115,188 @@ module mp_fer_hires
          return
       end if 
 
-      
-!      if (is_fer_mp_hires_advect) then
-!           CALL FER_HIRES_ADVECT(                                       &
-!                   ITIMESTEP=itimestep,DT=dt,DX=dx,DY=dy, GID=id        &
+!HWRF v4.0 version
+!           CALL FER_HIRES(                                              &
+!                   ITIMESTEP=itimestep,DT=dt, GID=id        &
 !                  ,RAINNC=rainnc,RAINNCV=rainncv                        &
 !                  ,DZ8W=dz8w,RHO_PHY=rho,P_PHY=p                        &
 !                  ,PI_PHY=pi_phy,TH_PHY=th                              &
 !                  ,QV=qv_curr                                           &
+!                  ,QT=qt_curr                                           &
 !                  ,LOWLYR=LOWLYR,SR=SR                                  &
-!                  ,QC=qc_curr,QR=Qr_curr,QI=Qi_curr,QRIMEF=qrimef_curr  &
+!                  ,F_ICE_PHY=F_ICE_PHY,F_RAIN_PHY=F_RAIN_PHY            &
+!                  ,F_RIMEF_PHY=F_RIMEF_PHY                              &
+!                  ,QC=qc_curr,QR=Qr_curr,QI=Qi_curr                     &
 !                  ,IDS=ids,IDE=ide, JDS=jds,JDE=jde, KDS=kds,KDE=kde    &
 !                  ,IMS=ims,IME=ime, JMS=jms,JME=jme, KMS=kms,KME=kme    &
 !                  ,ITS=its,ITE=ite, JTS=jts,JTE=jte, KTS=kts,KTE=kte    &
 !                  ,errmsg=errmsg, errflg=errflg )
-!      else 
 
 !-------------------------------------------------------------------
-!NAM: 'fer_hires': module_MICROPHYSICS.F90
-!*** Update the rime factor array after 3d advection
+!*** NAMPHYSICS below
 !-------------------------------------------------------------------
-!           DO K=1, LM
-!           DO J=JMS,JME
-!           DO I=IMS,IME
-!             IF  (QG(I,J,K)>EPSQ .AND. QS(I,J,K)>EPSQ) THEN
-!                F_RIMEF(I,J,K)=MIN(50.,MAX(1.,QG(I,J,K)/QS(I,J,K)))
-!             ELSE
-!                F_RIMEF(I,J,K)=1.
-!             ENDIF
-!           ENDDO
-!           ENDDO
-!           ENDDO
-          
+   
+        NTSD=ITIMESTEP
+        DTPHS=NPHS*DT
+!       RDTPHS=1./DTPHS
+!       AVRAIN=AVRAIN+1.
+       
 
-!HWRF
-           CALL FER_HIRES(                                              &
-!MZ                   ITIMESTEP=itimestep,DT=dt,DX=dx,DY=dy, GID=id        &
-                   ITIMESTEP=itimestep,DT=dt, GID=id        &
-                  ,RAINNC=rainnc,RAINNCV=rainncv                        &
-                  ,DZ8W=dz8w,RHO_PHY=rho,P_PHY=p                        &
-                  ,PI_PHY=pi_phy,TH_PHY=th                              &
-                  ,QV=qv_curr                                           &
-                  ,QT=qt_curr                                           &
+!zm:......................................................................
+!$omp parallel do                                                       &
+!$omp& private(j,i,k,ql,tl)
+!.......................................................................
+
+        DO J=JMS,JME
+        DO I=IMS,IME
+!
+           LOWLYR(I,J)=1
+!          XLAND(I,J)=SM(I,J)+1.   
+
+
+!------------------------------------------------------------------
+!*** FILL RAINNC WITH ZERO (NORMALLY CONTAINS THE NONCONVECTIVE
+!***                        ACCUMULATED RAIN BUT NOT YET USED BY NMM)
+!*** COULD BE OBTAINED FROM ACPREC AND CUPREC (ACPREC-CUPREC)
+!------------------------------------------------------------------
+!.. The NC variables were designed to hold simulation total accumulations
+!.. whereas the NCV variables hold timestep only values, so change below
+!.. to zero out only the timestep amount preparing to go into each 
+!.. micro routine while allowing NC vars to accumulate continually. 
+!.. But, the fact is, the total accum variables are local, never saved 
+!.. nor written so they go nowhere at the moment.
+!
+           RAINNC (I,J)=0. ! NOT YET USED BY NMM
+           RAINNCv(I,J)=0.
+           SNOWNCv(I,J)=0.
+           graupelncv(i,j) =0.0
+
+!-------------------------------------------------------------------
+!***  FILL THE SINGLE-COLUMN INPUT
+!-------------------------------------------------------------------
+!
+           DO K=LM,1,-1           ! We are moving down from the top in the flipped arrays
+!        
+!zm             TL(K)=T(I,J,K)
+!             QL(K)=AMAX1(Q(I,J,K),EPSQ)
+!
+          RR(I,J,K)=P_PHY(I,K)/(R_D*TL(K)*(P608*QL(K)+1.))
+          PI_PHY(I,J,K)=(P_PHY(I,K)*1.E-5)**CAPPA
+          TH_PHY(I,J,K)=TL(K)/PI_PHY(I,J,K)
+          DZ(I,J,K)=(PRSI(I,K+1)-PRSI(I,K))*R_G/RR(I,J,K)
+!
+        ENDDO    !- DO K=LM,1,-1
+!
+      ENDDO    !- DO I=IMS,IME
+      ENDDO    !- DO J=JMS,JME
+
+!zm: OMP.......................................................................
+!$omp end parallel do
+!.......................................................................
+
+             
+
+
+!-------------------------------------------------------------------
+!*** NOTE: THE NMMB HAS IJK STORAGE WITH LAYER 1 AT THE TOP.
+!*** THE WRF PHYSICS DRIVER HAVE IKJ STORAGE WITH LAYER 1
+!*** AT THE BOTTOM.
+!-------------------------------------------------------------------
+!
+! module_MICROPHYSICS.F90 in namephysics
+! Update the rime factor array after 3d advection
+        Do K =1, LM
+        DO J=JMS,JME
+        DO I=IMS,IME
+          IF (QG(I,J,K)>EPSQ  .AND. QS(I,J,K)>EPSQ) THEN
+             F_RIMEF(I,J,K)=MIN(50., MAX(1.,QG(I,J,K)/QS(I,J,K)))
+          ELSE 
+             F_RIMEF(I,J,K)=1.
+          ENDIF 
+        ENDDO
+        ENDDO
+        ENDDO
+
+!---------------------------------------------------------------------
+! *** CALL MICROPHYSICS
+
+            CALL FER_HIRES(                                             &
+                   ITIMESTEP=ntsd,DT=dtphs,RHgrd=RHGRD                  &
+                  ,DZ8W=dz,RHO_PHY=rr,P_PHY=p_phy,PI_PHY=pi_phy         &
+                  ,TH_PHY=th_phy                                        &
+                  ,Q=Q,QC=QC,QS=QS,QR=QR,QT=cwm                         &
                   ,LOWLYR=LOWLYR,SR=SR                                  &
-                  ,F_ICE_PHY=F_ICE_PHY,F_RAIN_PHY=F_RAIN_PHY            &
-                  ,F_RIMEF_PHY=F_RIMEF_PHY                              &
-                  ,QC=qc_curr,QR=Qr_curr,QI=Qi_curr                     &
-                  ,IDS=ids,IDE=ide, JDS=jds,JDE=jde, KDS=kds,KDE=kde    &
-                  ,IMS=ims,IME=ime, JMS=jms,JME=jme, KMS=kms,KME=kme    &
-                  ,ITS=its,ITE=ite, JTS=jts,JTE=jte, KTS=kts,KTE=kte    &
-                  ,errmsg=errmsg, errflg=errflg )
-         endif
+                  ,F_ICE_PHY=F_ICE,F_RAIN_PHY=F_RAIN                    &
+                  ,F_RIMEF_PHY=F_RIMEF                                  &
+                  ,RAINNC=rainnc,RAINNCV=rainncv                        &
+                  ,IMS=IMS,IME=IME,JMS=JMS,JME=JME,LM=LM                &
+                  ,D_SS=d_ss,MPRATES=mprates                            &
+                  ,refl_10cm=refl_10cm)
+
+!---------------------------------------------------------------------
+!*** Calculate graupel from snow array and rime factor
+!---------------------------------------------------------------------
+              DO K=1,LM
+              DO J=JMS,JME
+              DO I=IMS,IME
+                QG(I,J,K)=QS(I,J,K)*F_RIMEF(I,J,K)
+              ENDDO
+              ENDDO
+              ENDDO
+!---------------------------------------------------------------------
+
+!.......................................................................
+!$omp parallel do                                                       &
+!$omp& private(i,j,k,TNEW,MP_TTEN)
+!.......................................................................
+      DO K=1,LM
+        DO J=JMS,JME
+        DO I=IMS,IME
+!
+!-----------------------------------------------------------------------
+!***  UPDATE TEMPERATURE, SPECIFIC HUMIDITY, CLOUD WATER, AND HEATING.
+!-----------------------------------------------------------------------
+!
+          TNEW=TH_PHY(I,J,K)*PI_PHY(I,J,K)
+          TRAIN(I,J,K)=TRAIN(I,J,K)+(TNEW-T(I,J,K))*RDTPHS
+!         IF (USE_RADAR) THEN
+!           MP_TTEN=(TNEW-T(I,J,K))*RDTPHS
+!           IF(DFI_TTEN(I,J,K)>MP_TTEN.AND.DFI_TTEN(I,J,K)<0.01        &
+!                                     .AND.MP_TTEN<0.0018)THEN
+!             MP_TTEN=DFI_TTEN(I,J,K)
+!           END IF
+!           T(I,J,K)=T(I,J,K)+MP_TTEN/RDTPHS
+!         ELSE
+            T(I,J,K)=TNEW
+!         ENDIF
+        ENDDO
+        ENDDO
+      ENDDO
+!.......................................................................
+!$omp end parallel do
+!.......................................................................
+!
+!-----------------------------------------------------------------------
+!***  UPDATE PRECIPITATION
+!-----------------------------------------------------------------------
+!
+!jaa!$omp parallel do                                                       &
+!jaa!$omp& private(i,j,pcpcol)
+      DO J=JMS,JME
+      DO I=IMS,IME
+        PCPCOL=RAINNCV(I,J)*1.E-3
+        PREC(I,J)=PREC(I,J)+PCPCOL
+        ACPREC(I,J)=ACPREC(I,J)+PCPCOL
+!
+! NOTE: RAINNC IS ACCUMULATED INSIDE MICROPHYSICS BUT NMM ZEROES IT OUT ABOVE
+!       SINCE IT IS ONLY A LOCAL ARRAY FOR NOW
+!
+      ENDDO
+      ENDDO
+!
+
+
+
 
        end subroutine mp_fer_hires_run 
 
