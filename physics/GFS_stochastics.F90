c9557ec09ad7c5ec5b210577728cba62191a82d9
jeff beck <jeff.beck@noaa.gov>
2020-03-02 20:16:47 +0000

    Flip k dimension to correctly output all ad-hoc stochastic physics fields

--- physics/GFS_stochastics.F90
+++ physics/GFS_stochastics.F90
@@ -124,7 +124,7 @@
                if (use_zmtnblck)then
                   sppt_wts(i,k)=(sppt_wts(i,k)-1)*sppt_vwt+1.0
                endif
-               sppt_wts_inv(i,km-k+1)=sppt_wts(i,k)
+               sppt_wts_inv(i,k)=sppt_wts(i,k)
 
                !if(isppt_deep)then
 
@@ -190,7 +190,7 @@
          if (do_shum) then
            do k=1,km
              gq0(:,k) = gq0(:,k)*(1.0 + shum_wts(:,k))
-             shum_wts_inv(:,km-k+1) = shum_wts(:,k)
+             shum_wts_inv(:,k) = shum_wts(:,k)
            end do
          endif
          
@@ -198,8 +198,8 @@
            do k=1,km
              gu0(:,k) = gu0(:,k)+skebu_wts(:,k)*(diss_est(:,k))
              gv0(:,k) = gv0(:,k)+skebv_wts(:,k)*(diss_est(:,k))
-             skebu_wts_inv(:,km-k+1) = skebu_wts(:,k)
-             skebv_wts_inv(:,km-k+1) = skebv_wts(:,k)
+             skebu_wts_inv(:,k) = skebu_wts(:,k)
+             skebv_wts_inv(:,k) = skebv_wts(:,k)
            enddo
          endif
 
