1f8d0b78a53cd0d27a37d4086705bf27f373f36e
Yihua.Wu <yihua.wu@noaa.gov>
2020-06-16 22:59:45 +0000

    1. Added : flake_driv.F90, flake_driv.meta, flake.F90 2. Modified: GFS_time_vary_pre.fv3.F90 because Flake needs julian day 3. Modified: GFS_surface_composites.F90, GFS_surface_composites.meta added lae depth, and define the logical variable lake based on lakefraction and lakedepth Then, the logical variable lake is passed to flake drive and sfc_ocean. When lake is true, flake will be called; when lake is false, ocean model will be called 4. Modified: sfc_ocean.F, sfc_ocean.meta added lake, if not lake, calculation will be conducted.

--- physics/GFS_surface_composites.F90
+++ physics/GFS_surface_composites.F90
@@ -25,7 +25,7 @@ contains
 !! \htmlinclude GFS_surface_composites_pre_run.html
 !!
    subroutine GFS_surface_composites_pre_run (im, frac_grid, flag_cice, cplflx, cplwav2atm,                     &
-                                 landfrac, lakefrac, oceanfrac,                                                 &
+                                 landfrac, lakefrac, lakedepth, oceanfrac,                                      &
                                  frland, dry, icy, lake, ocean, wet, cice, cimin, zorl, zorlo, zorll, zorl_wat, &
                                  zorl_lnd, zorl_ice, snowd, snowd_wat, snowd_lnd, snowd_ice, tprcp, tprcp_wat,  &
                                  tprcp_lnd, tprcp_ice, uustar, uustar_lnd, uustar_ice, weasd, weasd_wat,        &
@@ -42,7 +42,7 @@ contains
       logical, dimension(im),              intent(in   ) :: flag_cice
       logical,              dimension(im), intent(inout) :: dry, icy, lake, ocean, wet
       real(kind=kind_phys),                intent(in   ) :: cimin
-      real(kind=kind_phys), dimension(im), intent(in   ) :: landfrac, lakefrac, oceanfrac
+      real(kind=kind_phys), dimension(im), intent(in   ) :: landfrac, lakefrac, oceanfrac, lakedepth
       real(kind=kind_phys), dimension(im), intent(inout) :: cice
       real(kind=kind_phys), dimension(im), intent(  out) :: frland
       real(kind=kind_phys), dimension(im), intent(in   ) :: zorl, snowd, tprcp, uustar, weasd
@@ -171,6 +171,14 @@ contains
             gflx_ice(i) = zero
            semis_ice(i) = 0.95d0
         endif
+      enddo
+! to prepare to separate lake from ocean in later
+      do i = 1, im
+          if(lakefrac(i) .ge. 0.15 .and. lakedepth(i) .gt. 1.0) then
+             lake(i) = .true.
+          else
+             lake(i) = .false.
+          endif
       enddo
 
      ! Assign sea ice temperature to interstitial variable
