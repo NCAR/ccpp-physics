219bc39f6214448a9575f7f0cb528ec315b8221d
Shan.Sun <shan.sun@noaa.gov>
2020-11-20 23:23:50 +0000

    Reverting changes in 7 lines.

--- physics/GFS_surface_composites.F90
+++ physics/GFS_surface_composites.F90
@@ -187,7 +187,7 @@ contains
         tprcp_lnd(i) = tprcp(i)
         tprcp_ice(i) = tprcp(i)
         if (wet(i)) then                   ! Water
-!         uustar_wat(i) = uustar(i)
+          uustar_wat(i) = uustar(i)
             zorl_wat(i) = zorlo(i)
             tsfc_wat(i) = tsfco(i)
            tsurf_wat(i) = tsfco(i)
@@ -196,8 +196,8 @@ contains
            weasd_wat(i) = zero
            snowd_wat(i) = zero
            semis_wat(i) = 0.984_kind_phys
-!            qss_wat(i) = qss(i)
-!           hflx_wat(i) = hflx(i)
+             qss_wat(i) = qss(i)
+            hflx_wat(i) = hflx(i)
         endif
         if (dry(i)) then                   ! Land
           uustar_lnd(i) = uustar(i)
@@ -207,8 +207,8 @@ contains
            tsurf_lnd(i) = tsfcl(i)
            snowd_lnd(i) = snowd(i)
            semis_lnd(i) = semis_rad(i)
-!            qss_lnd(i) = qss(i)
-!           hflx_lnd(i) = hflx(i)
+             qss_lnd(i) = qss(i)
+            hflx_lnd(i) = hflx(i)
         end if
         if (icy(i)) then                   ! Ice
           uustar_ice(i) = uustar(i)
@@ -220,8 +220,8 @@ contains
             ep1d_ice(i) = zero
             gflx_ice(i) = zero
            semis_ice(i) = 0.95_kind_phys
-!            qss_ice(i) = qss(i)
-!           hflx_ice(i) = hflx(i)
+             qss_ice(i) = qss(i)
+            hflx_ice(i) = hflx(i)
         endif
         if (nint(slmsk(i)) /= 1) slmsk(i)  = islmsk(i)
       enddo
