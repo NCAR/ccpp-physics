17ccdcf03fcf22d1c5df89e646e1b28c04160c2b
Yihua.Wu <yihua.wu@noaa.gov>
2021-03-18 22:27:32 +0000

    Updated sfc_nst.f to remove unnecessary statements

--- physics/sfc_nst.f
+++ physics/sfc_nst.f
@@ -9,14 +9,11 @@
 ! \brief This subroutine is empty since there are no procedures that need to be done to initialize the GFS NSST code.
 !! This subroutine is empty since there are no procedures that need to be done to initialize the GFS NSST code.
 !!
-!! \section arg_table_sfc_nst_init  Argument Table
-!!
       subroutine sfc_nst_init
       end subroutine sfc_nst_init
 
 ! \brief This subroutine is empty since there are no procedures that need to be done to finalize the GFS NSST code.
 !! This subroutine is empty since there are no procedures that need to be done to finalize the GFS NSST code.
-!! \section arg_table_sfc_nst_finalize  Argument Table
 !!
       subroutine sfc_nst_finalize
       end subroutine sfc_nst_finalize
@@ -661,13 +658,9 @@ cc
 !! surface in the GFS physics suite. The other two are the Noah land
 !! surface model and the sice simplified ice model.
 !!
-!! \section arg_table_sfc_nst_init  Argument Table
-!!
       subroutine sfc_nst_pre_init
       end subroutine sfc_nst_pre_init
 
-!! \section arg_table_sfc_nst_finalize  Argument Table
-!!
       subroutine sfc_nst_pre_finalize
       end subroutine sfc_nst_pre_finalize
 
