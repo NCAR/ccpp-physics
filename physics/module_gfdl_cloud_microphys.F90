02ee43319fd2160628c1cbf32a802e5cecb2376f
xiaqiong.zhou <Xiaqiong.Zhou@noaa.gov>
2021-03-27 01:05:10 +0000

    Comment out public statements not used

--- physics/module_gfdl_cloud_microphys.F90
+++ physics/module_gfdl_cloud_microphys.F90
@@ -47,9 +47,9 @@ module gfdl_cloud_microphys_mod
 
    public gfdl_cloud_microphys_mod_driver, gfdl_cloud_microphys_mod_init, &
           gfdl_cloud_microphys_mod_end, cloud_diagnosis
-   public wqs1, wqs2, qs_blend, wqsat_moist, wqsat2_moist
-   public qsmith_init, qsmith, es2_table1d, es3_table1d, esw_table1d
-   public setup_con, wet_bulb
+!   public wqs1, wqs2, qs_blend, wqsat_moist, wqsat2_moist
+!   public qsmith_init, qsmith, es2_table1d, es3_table1d, esw_table1d
+!   public setup_con, wet_bulb
 
    real :: missing_value = - 1.e10
 
