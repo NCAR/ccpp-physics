ff590a72d09b99dcd9c73a2df8004f9bd01fdc31
Dustin Swales <dustin.swales@noaa.gov>
2021-04-23 17:21:22 +0000

    Added call to compute_optimal_angle call before rte_lw(). New field passed into rte_lw().

--- physics/rrtmgp_lw_rte.F90
+++ physics/rrtmgp_lw_rte.F90
@@ -75,6 +75,7 @@ contains
     logical :: &
          top_at_1
     integer :: iSFC, iTOA
+    real(kind_phys), dimension(nCol,lw_gas_props%get_ngpt()) :: lw_Ds
 
     ! Initialize CCPP error handling variables
     errmsg = ''
@@ -106,13 +107,14 @@ contains
 
     ! Call RTE solver
     if (doLWclrsky) then
+       call check_error_msg('rrtmgp_lw_rte_run_opt_angle',lw_gas_props%compute_optimal_angles(lw_optical_props_clrsky,lw_Ds))
        call check_error_msg('rrtmgp_lw_rte_run',rte_lw(           &
             lw_optical_props_clrsky,         & ! IN  - optical-properties
             top_at_1,                        & ! IN  - veritcal ordering flag
             sources,                         & ! IN  - source function
             sfc_emiss_byband,                & ! IN  - surface emissivity in each LW band
             flux_clrsky,                     & ! OUT - Fluxes
-            n_gauss_angles = nGauss_angles))   ! IN  - Number of angles in Gaussian quadrature
+            lw_Ds = lw_Ds))
 
        ! Store fluxes
        fluxlwUP_clrsky   = sum(flux_clrsky%bnd_flux_up,dim=3)
