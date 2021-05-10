fd48665ef3e8938891df52579db4fcca0223d7a7
Xia Sun <xia.sun@noaa.gov>
2021-05-10 14:12:40 -0600

    Consistency check for MYJ sfclay scheme

--- physics/module_MYJSFC_wrapper.F90
+++ physics/module_MYJSFC_wrapper.F90
@@ -8,7 +8,22 @@
 
       contains
 
-      subroutine myjsfc_wrapper_init ()
+      subroutine myjsfc_wrapper_init (do_myjsfc,    &
+      & errmsg,errflg)
+
+        logical,          intent(in)  :: do_myjsfc
+        character(len=*), intent(out) :: errmsg
+        integer,          intent(out) :: errflg
+
+      ! Initialize CCPP error handling variables
+        errmsg = ''
+        errflg = 0
+
+      ! Consistency checks
+        if (.not. do_myjsfc) then
+          write(errmsg,fmt='(*(a))') 'Logic error: do_myjsfc = .false.'
+          errflg = 1
+          return
       end subroutine myjsfc_wrapper_init
 
       subroutine myjsfc_wrapper_finalize ()
