9a1dc8d8cdb4cce79c6d678ebb64b184310c694d
joeolson42 <Joseph.B.Olson@noaa.gov>
2021-07-12 20:03:04 +0000

    Bug fixes - removing a 3rd doxygen bug

--- physics/module_bl_mynn.F90
+++ physics/module_bl_mynn.F90
@@ -7535,7 +7535,7 @@ SUBROUTINE SCALE_AWARE(dx,PBL1,Psig_bl,Psig_shcu)
   FUNCTION phim(zet)
     !! New stability function parameters for momentum (Puhales, 2020, WRF 4.2.1)
     !! The forms in unstable conditions (z/L < 0) use Grachev et al. (2000), which are a blend of 
-    !! the classical “Kansas” forms (i.e., Paulson 1970, Dyer and Hicks 1970), valid for weakly 
+    !! the classical (Kansas) forms (i.e., Paulson 1970, Dyer and Hicks 1970), valid for weakly 
     !! unstable conditions (−1 < z/L < 0). The stability functions for stable conditions use an 
     !! updated form taken from Cheng and Brutsaert (2005), which extends the validity into very 
     !! stable conditions [z/L ~ O(10)].
