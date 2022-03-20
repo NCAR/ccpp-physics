96f58e021a1d5607a446dd0826982343929c72d5
weizhong zheng <weizhong.zheng@noaa.gov>
2022-03-20 14:07:49 +0000

    tuning cd/lm parameter

--- physics/module_sf_noahmplsm.f90
+++ physics/module_sf_noahmplsm.f90
@@ -4686,7 +4686,7 @@ endif   ! croptype == 0
             fhgh = 0.5 * (fhgh+fhgnewh)
        endif
 
-       cwpc = (cwp * vai * hcan * fhg)**0.5
+       cwpc = (0.5 * cwp * vai * hcan * fhg)**0.5
 !       cwpc = (cwp*fhg)**0.5
 
        tmp1 = exp( -cwpc*z0hg/hcan )
@@ -4695,7 +4695,7 @@ endif   ! croptype == 0
 
 ! aerodynamic resistances raw and rah between heights zpd+z0h and z0hg.
 
-       kh  = max ( 0.5*vkc*fv*(hcan-zpd)/fhgh, mpe )
+       kh  = max ( vkc*fv*(hcan-zpd)/fhgh, mpe )
        ramg = 0.
        rahg = tmprah2 / kh
        rawg = rahg
