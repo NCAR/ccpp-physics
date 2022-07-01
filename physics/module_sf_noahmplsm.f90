70cdc31c664c5749b1088d9bff835ab332ab7bd8
samuel.trahan <Samuel.Trahan@noaa.gov>
2022-07-01 14:33:35 +0000

    correct a type mismatch in a call in module_sf_noahmplsm

--- physics/module_sf_noahmplsm.f90
+++ physics/module_sf_noahmplsm.f90
@@ -10823,6 +10823,7 @@ end subroutine psn_crop
       real (kind=kind_phys) :: x1,x2,fx1,fx2
       integer :: n
       integer, parameter :: nmax = 20
+      real(kind=kind_phys) rphys_temp
       !real, dimension(nmax):: zlhux
 !     real  :: zolri2
 
@@ -10855,7 +10856,9 @@ end subroutine psn_crop
 
       if (n==nmax .and. abs(x1 - x2) >= 0.01) then
          !if convergence fails, use approximate values:
-         call li_etal_2010(zolri, ri, za/z0, z0/zt)
+         rphys_temp = zolri
+         call li_etal_2010(rphys_temp, ri, za/z0, z0/zt)
+         zolri = rphys_temp
          !zlhux(n)=zolri
          !print*,"iter fail, n=",n," ri=",ri," z0=",z0
       else
@@ -10921,7 +10924,7 @@ end subroutine psn_crop
       integer :: n
       integer, parameter :: nmax = 20
       real (kind=kind_phys), dimension(nmax):: zlhux
-      real (kind=kind_phys) :: psit2,psix2
+      real (kind=kind_phys) :: psit2,psix2,phys_temp
 
 !     real    :: psim_unstable, psim_stable
 !     real    :: psih_unstable, psih_stable
@@ -10972,14 +10975,18 @@ end subroutine psn_crop
       if (n==nmax .and. abs(zolold - zolrib) > 0.01 ) then
          !print*,"iter fail, n=",n," ri=",ri," z/l=",zolri
          !if convergence fails, use approximate values:
-         call li_etal_2010(zolrib, ri, za/z0, z0/zt)
+         phys_temp = zolrib
+         call li_etal_2010(phys_temp, ri, za/z0, z0/zt)
+         zolrib = phys_temp
          zlhux(n)=zolrib
          !print*,"failed, n=",n," ri=",ri," z0=",z0
          !print*,"z/l=",zlhux(1:nmax)
       else
          !if(zolrib*ri .lt. 0.) then
          !   !print*,"end: wrong quadrants: z/l=",zolrib," ri=",ri
+         !   !phys_temp = zolrib
          !   !call li_etal_2010(zolrib, ri, za/z0, z0/zt)
+         !   !zolrib = phys_temp
          !endif
          !print*,"success,n=",n," ri=",ri," z0=",z0
       endif
