b9f1087747d3f4b775605649691c405236ae723e
samuel.trahan <Samuel.Trahan@noaa.gov>
2023-04-04 04:43:12 +0000

    skip clm_lake_run if there are no lake points in the thread

--- physics/clm_lake.f90
+++ physics/clm_lake.f90
@@ -472,7 +472,7 @@ MODULE clm_lake
       errmsg = ' '
       errflg = 0
 
-      if(iopt_lake/=iopt_lake_clm .or. lkm==0) then
+      if(iopt_lake/=iopt_lake_clm .or. lkm==0 .or. all(.not.use_lake_model)) then
         return ! nothing to do
       endif
 
