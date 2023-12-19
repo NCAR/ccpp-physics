a9208405adffd6b1b6389ac3f1331097b05069ad
Jili Dong <Jili.Dong@noaa.gov>
2023-12-19 16:45:32 +0000

    remove excess whitespace

--- physics/clm_lake.f90
+++ physics/clm_lake.f90
@@ -268,7 +268,7 @@ MODULE clm_lake
          ! Atmospheric model state inputs:
          tg3, pgr, zlvl, gt0, prsi, phii, qvcurr, gu0, gv0, xlat_d, xlon_d,       &
          ch, cm, dlwsfci, dswsfci, oro_lakedepth, wind, rho0, tsfc,               &
-         flag_iter, flag_lakefreeze, ISLTYP, rainncprv, raincprv,                     &
+         flag_iter, flag_lakefreeze, ISLTYP, rainncprv, raincprv,                 &
 
          ! Feedback to atmosphere:
          evap_wat,     evap_ice,   hflx_wat,    hflx_ice,  gflx_wat, gflx_ice,    &
