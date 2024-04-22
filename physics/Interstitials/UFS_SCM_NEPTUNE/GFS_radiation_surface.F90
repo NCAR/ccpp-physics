240e3cf886e83497c002ec52137d1b649e6fbc41
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-04-22 14:10:14 -0600

    Add optional in physics/Interstitials/UFS_SCM_NEPTUNE/GFS_radiation_surface.F90

--- physics/Interstitials/UFS_SCM_NEPTUNE/GFS_radiation_surface.F90
+++ physics/Interstitials/UFS_SCM_NEPTUNE/GFS_radiation_surface.F90
@@ -72,16 +72,17 @@
       integer, dimension(:), intent(in) :: use_lake_model
 
       real(kind=kind_phys), dimension(:),   intent(in)  :: xlat, xlon, slmsk,           &
-                                                           sfc_alb_pert, lndp_prt_list, &
+                                                           sfc_alb_pert, &
                                                            landfrac, lakefrac,          &
                                                            snodl, snodi, sncovr,        &
                                                            sncovr_ice, fice, zorl,      &
                                                            hprime, tsfg, tsfa, tisfc,   &
                                                            coszen, alvsf, alnsf, alvwf, &
                                                            alnwf, facsf, facwf, snoalb
-      character(len=3)    , dimension(:),   intent(in)  :: lndp_var_list
-      real(kind=kind_phys), dimension(:),   intent(in)  :: albdvis_ice, albdnir_ice,    &
-                                                           albivis_ice, albinir_ice
+      real(kind=kind_phys), dimension(:),   intent(in), optional :: lndp_prt_list
+      character(len=3)    , dimension(:),   intent(in), optional :: lndp_var_list
+      real(kind=kind_phys), dimension(:),   intent(in), optional :: albdvis_ice, albdnir_ice,    &
+                                                                    albivis_ice, albinir_ice
 
       real(kind=kind_phys), dimension(:),   intent(inout) :: albdvis_lnd, albdnir_lnd,  &
                                                              albivis_lnd, albinir_lnd,  &
