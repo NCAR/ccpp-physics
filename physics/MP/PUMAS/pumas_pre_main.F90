54bbdc28fe20cc6a8fbca9949a414702b7d249d1
Jesse Nusbaumer <nusbaume@ucar.edu>
2025-01-09 15:18:18 -0700

    Add PUMAS directory with stub interstitial file.

---
+++ physics/MP/PUMAS/pumas_pre_main.F90
@@ -0,0 +1,21 @@
+!This is just a placeholder for the eventual CCPP
+!interstitial schemes needed for the PUMAS cloud
+!microphysics package.
+
+!The hope that there will eventually be one set of
+!portable interstitials and possibly another set
+!of host-specific intersititals (if needed) above
+!the portable layer.
+
+module pumas_pre_main
+
+   implicit none
+
+   contains
+
+   !Add subroutines here
+   !for any pre-processing
+   !steps needed before the core
+   !PUMAS calls.
+
+end module pumas_pre_main
