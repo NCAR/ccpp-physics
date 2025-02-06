f32a01e2f4b1b2ab52608dae0597ccebbc4aaf4f
larissa.reames@noaa.gov <larissa.reames@noaa.gov>
2025-02-06 21:31:35 +0000

    Revert "update module_sf_noahmplsm.F90"
    This reverts commit 6619587ae9d6ecd5c9f45b24da0a48cf40648e8b.

--- physics/SFC_Models/Land/Noahmp/module_sf_noahmplsm.F90
+++ physics/SFC_Models/Land/Noahmp/module_sf_noahmplsm.F90
@@ -3024,7 +3024,7 @@ endif   ! croptype == 0
   
   call snow_age (parameters,dt,tg,sneqvo,sneqv,tauss,fage) 
   
-  if(cosz > 0) then 
+  if(cosz > 0) 
 
 ! weight reflectance/transmittance by lai and sai
 
@@ -3043,6 +3043,7 @@ endif   ! croptype == 0
   if(opt_alb == 2) then
      call snowalb_class (parameters,nband,qsnow,dt,alb,albold,albsnd,albsni,iloc,jloc)
      albold = alb
+  end if
   end if
 
 ! ground surface albedo
@@ -3084,7 +3085,8 @@ endif   ! croptype == 0
      wl = ext 
   end if
   fsun = wl
-  end if
+
+100 continue
 
   end subroutine albedo
 
