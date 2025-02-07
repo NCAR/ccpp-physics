1e5a6d3af61eae38a6e431d47548e7a33e54893f
Grant Firl <grant.firl@noaa.gov>
2025-02-07 10:06:44 -0500

    remove changes not in ufs/dev PR#225

--- physics/Radiation/RRTMG/rrtmg_sw_post.F90
+++ physics/Radiation/RRTMG/rrtmg_sw_post.F90
@@ -14,7 +14,7 @@
                  swhtr, sfcalb1, sfcalb2, sfcalb3, sfcalb4, htswc, htsw0,      &
                  nirbmdi, nirdfdi, visbmdi, visdfdi, nirbmui, nirdfui, visbmui,&
                  visdfui, sfcdsw, sfcnsw, htrsw, swhc, scmpsw, sfcfsw, topfsw, &
-                 sfcdswc, errmsg, errflg)
+                 errmsg, errflg)
 
       use machine,                   only: kind_phys
       use module_radsw_parameters,   only: topfsw_type, sfcfsw_type,   &
@@ -33,8 +33,7 @@
                                                              visbmdi, visdfdi, &
                                                              nirbmui, nirdfui, &
                                                              visbmui, visdfui, &
-                                                             sfcdsw,  sfcnsw,  &
-                                                             sfcdswc
+                                                             sfcdsw,  sfcnsw
       real(kind=kind_phys), dimension(:,:), intent(inout) :: htrsw, swhc
 
       type(cmpfsw_type), dimension(:),     intent(inout) :: scmpsw
@@ -123,7 +122,6 @@
         do i=1,im
           sfcnsw(i) = sfcfsw(i)%dnfxc - sfcfsw(i)%upfxc
           sfcdsw(i) = sfcfsw(i)%dnfxc
-          sfcdswc(i)= sfcfsw(i)%dnfx0
         enddo
 
       endif                                ! end_if_lsswr
