8b514281af8afdfef43ee2e4558c6d1340b540f4
Tracy <hertneky@ucar.edu>
2025-05-06 16:50:23 -0600

    another thing missed

--- physics/CONV/SAMF/samfshalcnv.f
+++ physics/CONV/SAMF/samfshalcnv.f
@@ -91,7 +91,9 @@
      &   cnvw(:,:), cnvc(:,:), dt_mf(:,:)
 !
       real(kind=kind_phys), intent(out) :: ud_mf(:,:)
-      real(kind=kind_phys), intent(inout), optional :: sigmaout(:,:)
+      real(kind=kind_phys), intent(inout), optional :: sigmaout(:,:),   &
+     &   omegaout(:,:)
+
       real(kind=kind_phys), intent(in) :: clam,    c0s,     c1,         &
      &                     asolfac, evef, pgcon
       logical,          intent(in)  :: hwrf_samfshal,first_time_step,   &
